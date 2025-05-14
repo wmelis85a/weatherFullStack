c8afcd90699ed59268fe834b43b0e2d072f235ad
wmelis85a <wmelis.85a@gmail.com>
2025-05-13 23:33:12 -0300

    adds render domain for CORS

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from app.api import weather
 from fastapi.middleware.cors import CORSMiddleware
 
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite port
+    allow_origins=["http://localhost:5173",    
+                "https://weatherfullstack-front.onrender.com"]
     allow_methods=["*"],
     allow_headers=["*"],
 )
