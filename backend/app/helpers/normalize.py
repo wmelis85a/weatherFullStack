37c3bb6407a03b4810616cd80a279705362b89ac
wmelis85a <wmelis.85a@gmail.com>
2026-09-18 20:46:24 -0300

    fix(DEV): adds error handling fixes

--- backend/app/helpers/normalize.py
+++ backend/app/helpers/normalize.py
@@ -6,5 +6,7 @@ def normalize_city_name(name: str) -> str:
     name = unicodedata.normalize("NFD", name)
     name = "".join(c for c in name if unicodedata.category(c) != "Mn")
 
+    print(f"Normalized city name: {name}")
+
     # removes trailing spaces and applies format
     return name.strip()
